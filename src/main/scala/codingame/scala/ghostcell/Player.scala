5e326d0e025eb8f76160ed365d1847f9fb1a3f9e
hwang <hwang@murex.com>
2017-03-08 13:07:54 +0100

    improve input reading

--- src/main/scala/codingame/scala/ghostcell/Player.scala
+++ src/main/scala/codingame/scala/ghostcell/Player.scala
@@ -2,33 +2,24 @@ package codingame.scala.ghostcell
 
 import codingame.scala.graph.{Edge, Itinerary, ShortestPath}
 
-/**
-  * Auto-generated code below aims at helping you parse
-  * the standard input according to the problem statement.
-  **/
 object Player extends App {
-  val factorycount = io.StdIn.readInt()
-  // the number of factories
-  val linkcount = io.StdIn.readInt() // the number of links between factories
+  val factoryCount = io.StdIn.readInt()
+  val edgeCount = io.StdIn.readInt()
 
-  var links: Vector[Edge] = (for {
-    i <- 0 until linkcount
+  var edges: Vector[Edge] = (for {
+    i <- 0 until edgeCount
     Array(factory1, factory2, distance) = for (i <- io.StdIn.readLine() split " ") yield i.toInt
-    edge1 = Edge(factory1, factory2, distance)
-    edge2 = Edge(factory2, factory1, distance)
-  } yield Vector(edge1, edge2)).toVector.flatten
+  } yield Edge(factory1, factory2, distance)).toVector
 
-  private val itinearies: Map[Int, Map[Int, Itinerary]] = ShortestPath.shortestItinearies(factorycount, links)
+  private val itinearies: Map[Int, Map[Int, Itinerary]] = GhostCellGrahp.shortestPath(factoryCount, edges)
 
   private val player = GhostCellPlayer
 
-  // game loop
   while (true) {
 
-    val entitycount = io.StdIn.readInt()
-    // the number of entities (e.g. factories and troops)
+    val entityCount = io.StdIn.readInt()
     val entities = (for {
-      i <- 0 until entitycount
+      i <- 0 until entityCount
       Array(entityid, entitytype, arg1, arg2, arg3, arg4, arg5) = io.StdIn.readLine().split(" ").map(_.toInt)
     } yield Entity(entityid, entitytype, arg1, arg2, arg3, arg4, arg5)).toVector
 
@@ -43,7 +34,6 @@ object Player extends App {
 
     val state = GhostCellGameState(itinearies, factories, troops, bombs)
 
-    // Any valid action, such as "WAIT" or "MOVE source destination cyborgs"
     val actions = player.reactTo(state)
     if (actions.isEmpty) {
       println(WaitAction.command())
