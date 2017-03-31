906a4acbb9b4c637f7b1035457310341774f0974
Hui Wang <hedonplay@gmail.com>
2017-03-31 22:28:43 +0200

    evaluate bot offline and compute the stats

--- src/main/scala/com/truelaurel/codingame/engine/Game.scala
+++ src/main/scala/com/truelaurel/codingame/engine/Game.scala
@@ -36,6 +36,30 @@ object GameSimulator {
     (0 until round).foldLeft(from)((s, r) => arena.next(s, players.flatMap(_.reactTo(s))))
   }
 
+
+  def evaluateOffline[S, A](games: Vector[S], arena: GameArena[S, A], players: Vector[GamePlayer[S, A]], round: Int = 200): Unit = {
+    val results = games.par.map(play(_, arena, players, round))
+
+    val wins = results.count {
+      case WinKO | WinTech => true
+      case _ => false
+    }
+
+    val draws = results.count {
+      case Draw => true
+      case _ => false
+    }
+
+    val p = wins.toDouble / games.size
+    val winRate = p * 100.0
+
+    val confidenceInterval = Math.sqrt(p * (1 - p) / games.size) * 100
+
+    println(s"${games.size} Games $wins Wins $draws Draws")
+    println(s"WinRate:$winRate%+-$confidenceInterval%")
+
+  }
+
   def play[S, A](from: S, arena: GameArena[S, A], players: Vector[GamePlayer[S, A]], round: Int = 200): GameResult = {
     val result = arena.judge(from)
     if (round == 0) result else {
