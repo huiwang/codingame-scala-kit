a1f80eac97b63413ac6f9d283099f44e619d2758
Hui Wang <hedonplay@gmail.com>
2017-04-14 08:28:56 +0200

    refactor disk collider

--- src/main/scala/com/truelaurel/codingame/collision/Disk.scala
+++ src/main/scala/com/truelaurel/codingame/collision/Disk.scala
@@ -13,28 +13,41 @@ object DiskMover {
 
 object DiskCollider extends Collider[Disk] {
   override def collideTime(d1: Disk, d2: Disk): Option[Double] = {
-    val dr = d2.p - d1.p
-    val dv = d2.v - d1.v
+    collideTime(d1.p, d1.v, d1.r, d2.p, d2.v, d2.r)
+  }
+
+  def collideTime(p1: Vectorl, v1: Vectorl, r1: Double,
+                  p2: Vectorl, v2: Vectorl, r2: Double): Option[Double] = {
+    val dr = p2 - p1
+    val dv = v2 - v1
     val dvdr = dv.dotProduct(dr)
     if (dvdr > 0) return None
     val dvdv = dv.dotProduct(dv)
     if (dvdv == 0) return None
     val drdr = dr.dotProduct(dr)
-    val sigma = d1.r + d2.r
+    val sigma = r1 + r2
     val d = (dvdr * dvdr) - dvdv * (drdr - sigma * sigma)
     if (d < 0) return None
     Some(-(dvdr + Math.sqrt(d)) / dvdv)
   }
 
-  override def bounceOff(d1: Disk, d2: Disk): (Disk, Disk) = {
-    val dr = d2.p - d1.p
-    val dv = d2.v - d1.v
+
+  def bounceOff(p1: Vectorl, v1: Vectorl, m1: Double,
+                p2: Vectorl, v2: Vectorl, m2: Double): (Vectorl, Vectorl) = {
+    val dr = p2 - p1
+    val dv = v2 - v1
     val drdr = dr.dotProduct(dr)
     val dvdr = dr.dotProduct(dv)
-    val massCoefficient = (d1.m + d2.m) / (d1.m * d2.m)
+    val massCoefficient = (m1 + m2) / (m1 * m2)
 
     val impulse = dr * 2.0 * dvdr / (massCoefficient * drdr)
-    (d1.copy(v = d1.v + impulse / d1.m), d2.copy(v = d2.v - impulse / d2.m))
+    (v1 + impulse / m1, v2 - impulse / m2)
+  }
+
+
+  override def bounceOff(d1: Disk, d2: Disk): (Disk, Disk) = {
+    val (v1, v2) = bounceOff(d1.p, d1.v, d1.m, d2.p, d2.v, d2.m)
+    (d1.copy(v = v1), d2.copy(v = v2))
   }
 
   override def move(disk: Disk, time: Double): Disk = DiskMover.move(disk, time)
