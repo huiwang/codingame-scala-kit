ada633ebba6696a20e6b4ca9034a9794ca40c75f
Hui Wang <hedonplay@gmail.com>
2017-04-20 08:18:51 +0200

    remove mine evolution

--- src/main/scala/com/truelaurel/codingame/caribbean/online/CaribbeanController.scala
+++ src/main/scala/com/truelaurel/codingame/caribbean/online/CaribbeanController.scala
@@ -48,18 +48,14 @@ object CaribbeanController extends GameController[CaribbeanContext, CaribbeanSta
 
 
   override def nextContext(context: CaribbeanContext, state: CaribbeanState, actions: Vector[CaribbeanAction]): CaribbeanContext = {
-    val mines = actions.flatMap {
-      case MineAction(shipId) => Some(shipId -> state.turn)
-      case _ => None
-    }.toMap
-
     val fires = actions.flatMap {
       case Fire(shipId, _) => Some(shipId -> state.turn)
       case _ => None
     }.toMap
 
-    CaribbeanContext(context.lastMine ++ mines, context.lastFire ++ fires)
+    CaribbeanContext(context.lastMine, context.lastFire ++ fires)
   }
+
 
   override def warmup(player: GamePlayer[CaribbeanState, CaribbeanAction]): Unit = {
     val state = CaribbeanState(CaribbeanContext(Map(), Map()), Vector(Ship(0, Offset(2, 6), 1, 0, 100, 0), Ship(1, Offset(2, 14), 5, 0, 100, 1), Ship(2, Offset(8, 3), 4, 0, 100, 0), Ship(3, Offset(8, 17), 2, 0, 100, 1), Ship(4, Offset(16, 2), 5, 0, 100, 0), Ship(5, Offset(16, 18), 1, 0, 100, 1)), Vector(Barrel(12, Offset(18, 10), 10), Barrel(14, Offset(12, 12), 15), Barrel(13, Offset(12, 8), 15), Barrel(16, Offset(11, 14), 17), Barrel(15, Offset(11, 6), 17), Barrel(18, Offset(3, 17), 11), Barrel(17, Offset(3, 3), 11), Barrel(20, Offset(1, 14), 11), Barrel(19, Offset(1, 6), 11), Barrel(22, Offset(13, 18), 15), Barrel(21, Offset(13, 2), 15), Barrel(24, Offset(9, 12), 15), Barrel(23, Offset(9, 8), 15)), Vector(), Vector(Mine(7, Offset(2, 16)), Mine(9, Offset(20, 16))), 1)
