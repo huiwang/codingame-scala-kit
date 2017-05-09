edd44bbe8482b541d0f3e9c13235cfa3ef82be29
[a518291] Michel Daviot <michel.daviot@atos.net>
2017-05-09 11:52:50 +0200

    split BundlerIo

--- src/main/scala/com/truelaurel/codingame/bundle/Bundler.scala
+++ src/main/scala/com/truelaurel/codingame/bundle/Bundler.scala
@@ -1,12 +1,6 @@
 package com.truelaurel.codingame.bundle
 
-import java.io.{File, PrintWriter}
-import java.nio.file.{Files, Paths}
-import java.util.Objects
-
-import scala.collection.mutable
-import scala.io.Source
-import scala.util.control.NonFatal
+import java.io.File
 
 object BundlerMain extends App {
   if (args.isEmpty) {
@@ -14,29 +8,28 @@ object BundlerMain extends App {
     sys.exit(1)
   }
   args.foreach { fileName =>
-    new Bundler(fileName, BundlerIo).bundle()
+    new Bundler(fileName, StdBundlerIo()).bundle()
   }
 }
 
-class Bundler(val fileName: String,
-              io: BundlerIo,
-              val srcFolder: String = "./src/main/scala",
-              val destFolder: String = "./target") {
-
-  import Bundler._
-
-  val fileContent = mutable.Map.empty[File, List[String]]
+class Bundler(fileName: String, io: BundlerIo) {
+  val ignoredImports = Seq("scala", "java")
 
   def bundle(): Unit = {
     val outputFileContent = buildOutput
-    val destFile = new File(destFolder, fileName)
-    io.save(destFile, outputFileContent)
+    io.save(fileName, outputFileContent)
   }
 
   def buildOutput: String = {
     val file = io.findFile(fileName)
     val content = transformFile(file)
     stripComments(content.mkString("\n"))
+  }
+
+  def dependentFiles(file: File, fileLines: List[String]): List[File] = {
+    val filesInSameFolder = io.filesInFolder(file.getParentFile)
+    val filesFromImport = fileLines.flatMap(filesFromLine)
+    filesFromImport ++ filesInSameFolder
   }
 
   def filesList(queue: List[File], contents: Map[File, List[String]]): List[File] = queue match {
@@ -47,12 +40,6 @@ class Bundler(val fileName: String,
       val nextContents = contents + (file -> fileLines)
       val nextQueue = (needed ++ t).distinct.diff(nextContents.keySet.toSeq)
       filesList(nextQueue, nextContents) ++ needed :+ file
-  }
-
-  def dependentFiles(file: File, fileLines: List[String]): List[File] = {
-    val filesInSameFolder = io.filesInFolder(file.getParentFile)
-    val filesFromImport = fileLines.flatMap(filesFromLine)
-    filesFromImport ++ filesInSameFolder
   }
 
   def transformFile(file: File): List[String] = {
@@ -67,61 +54,9 @@ class Bundler(val fileName: String,
     else {
       val imported = line.split(" ").tail.mkString
       val packageElements = imported.replaceAll("_", "").replaceAll("\\{.*\\}", "").split("\\.")
-      val subFolder = io.findFolder(packageElements, new File(srcFolder))
+      val subFolder = io.findFolder(packageElements)
       io.filesInFolder(subFolder)
     }
-}
-
-trait BundlerIo {
-  def readFile(file: File): List[String] = {
-    println(s"reading from $file")
-    try {
-      Source.fromFile(file).getLines().toList
-    } catch {
-      case NonFatal(e) =>
-        println("Error while reading file " + file)
-        e.printStackTrace()
-        throw e
-    }
-  }
-
-  def findFolder(packageElements: Array[String], rootFolder: File): File = {
-    packageElements.foldLeft(rootFolder) {
-      case (folder, pkg) =>
-        val f = new File(folder, pkg)
-        //TODO : could only import files listed in { cl1, cl2 }
-        if (f.isDirectory) f else folder
-    }
-  }
-
-  def save(destFile: File, content: String): Unit = {
-    val pw = new PrintWriter(destFile)
-    try {
-      println(s"writing to $destFile")
-      pw.write(content)
-    } finally pw.close()
-
-  }
-
-  def findFile(fileName: String): File = {
-    Files.find(Paths.get("."), Int.MaxValue, (path, _) => path.endsWith(fileName))
-      .findAny()
-      .orElseThrow(() => new IllegalArgumentException(s"$fileName not found"))
-      .toFile
-  }
-
-  def filesInFolder(folder: File): List[File] = {
-    Objects.requireNonNull(folder, "Folder should not be null")
-    val files = folder.listFiles((pathname: File) => !pathname.getName.startsWith("."))
-    Objects.requireNonNull(files, "visibleFiles should not be null in folder " + folder)
-    files.filterNot(_.isDirectory).toList.sortBy(_.getAbsolutePath)
-  }
-}
-
-object BundlerIo extends BundlerIo
-
-object Bundler {
-  val ignoredImports = Seq("scala", "java")
 
   def stripComments(x: String, s: String = "/*", e: String = "*/"): String = {
     val a = x indexOf s
