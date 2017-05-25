5a7c3dd3ef26ef9facd45cca40c554aa39d5ac9e
Hui Wang <hedonplay@gmail.com>
2017-05-25 18:37:45 +0200

    run one sample benchmark in ci

---
+++ src/main/scala/com/truelaurel/codingame/benchmark/HelloWorldBenchmark.scala
@@ -0,0 +1,12 @@
+package com.truelaurel.codingame.benchmark
+
+import org.openjdk.jmh.annotations.Benchmark
+
+class HelloWorldBenchmark {
+
+  @Benchmark
+  def wellHelloThere(): Unit = {
+    // this method was intentionally left blank.
+  }
+
+}
