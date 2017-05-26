146cab838da7450db993d616cac05e951faf1ac6
[a518291] Michel Daviot <michel.daviot@atos.net>
2017-05-26 17:46:11 +0200

    basic unit tests for Bundler

--- build.sbt
+++ build.sbt
@@ -1,6 +1,9 @@
 name := "CodinGame-Scala-Kit"
 version := "0.1.0"
 scalaVersion := "2.12.0"
+
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.1" % "test"
+
+libraryDependencies += "org.mockito"  % "mockito-core" % "2.8.9" % "test"
 
 enablePlugins(JmhPlugin)
--- src/test/scala/com/truelaurel/codingame/bundler/BundlerTest.scala
+++ src/test/scala/com/truelaurel/codingame/bundler/BundlerTest.scala
@@ -1,6 +1,11 @@
 package com.truelaurel.codingame.bundler
 
-import com.truelaurel.codingame.bundle.{Bundler, StdBundlerIo}
+import java.io.File
+
+import com.truelaurel.codingame.bundle.{Bundler, BundlerIo, StdBundlerIo}
+import org.mockito.ArgumentMatchers._
+import org.mockito.Mockito._
+import org.scalatest.mockito.MockitoSugar
 import org.scalatest.{FlatSpec, Matchers}
 
 import scala.io.Source
@@ -8,9 +13,79 @@ import scala.io.Source
 /**
   * Created by tyrcho on 07/05/2017.
   */
-class BundlerTest extends FlatSpec with Matchers {
+class BundlerTest extends FlatSpec with Matchers with MockitoSugar {
 
   behavior of "Bundler"
+
+  it should "copy simple content" in {
+    val inputName = "Demo.scala"
+    val content = "object Demo extends App"
+    val io = prepareMock(Map(inputName -> content))
+    val output = Bundler(inputName, io).buildOutput
+    output shouldBe content
+  }
+
+
+  it should "copy also file in same folder" in {
+    val inputName = "Demo.scala"
+    val content = "object Demo extends App"
+    val utilName = "Util.scala"
+    val utilContent = "object Util { def abs(x) = if(x>0) x else -x }"
+    val io = prepareMock(Map(
+      inputName -> content,
+      utilName -> utilContent))
+    val output = Bundler(inputName, io).buildOutput
+    output shouldBe content + "\n" + utilContent
+  }
+
+
+//  it should "resolve import from another package" in {
+//    val inputName = "Demo.scala"
+//    val content =
+//      """import util.Util
+//        |object Demo extends App
+//      """.stripMargin
+//    val utilName = "util/Util.scala"
+//    val utilContent =
+//      """package util
+//        |object Util { def abs(x) = if(x>0) x else -x }
+//        |""".stripMargin
+//    val io = prepareMock(Map(
+//      inputName -> content,
+//      utilName -> utilContent))
+//    val output = Bundler(inputName, io).buildOutput
+//    output shouldBe content + "\n" + utilContent
+//  }
+
+
+  private def prepareMock(fileContents: Map[String, String]): BundlerIo = {
+    val io = mock[BundlerIo]
+    val folder = new File(".")
+    for {
+      (pathName, content) <- fileContents
+    } {
+      val inputFile = new File(folder, pathName)
+      val path = pathName.split("/").init.mkString("/")
+      when(io.findFile(pathName)).thenReturn(inputFile)
+      when(io.filesInFolder(folder)).thenReturn(
+        fileContents.keys.toList
+          .filter(_.startsWith(path))
+          .map(n => new File(folder, n)))
+      when(io.readFile(inputFile)).thenReturn(content.split("""\r\n|\n""").toList)
+      when(io.findFolder(notNull[Array[String]])).thenAnswer(invocation =>
+        new File(invocation.getArgument[Array[String]](0).init.mkString("/")))
+
+    }
+
+    io
+  }
+
+  def findFolder(packageElements: Array[String]): File = {
+    packageElements.foldLeft(new File(".")) {
+      case (folder, pkg) => new File(folder, pkg)
+    }
+  }
+
 
   it should "have same output for GhostInTheCell" in {
     checkOuput(
