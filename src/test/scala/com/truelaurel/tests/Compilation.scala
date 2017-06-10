6b801e079b644a49a313e7246b667e3d0c772f1d
Hui Wang <hedonplay@gmail.com>
2017-06-10 14:05:25 +0200

    use scala reflection toolbox to validate generated code

--- src/test/scala/com/truelaurel/tests/Compilation.scala
+++ src/test/scala/com/truelaurel/tests/Compilation.scala
@@ -1,24 +1,20 @@
 package com.truelaurel.tests
 
-import java.io.File
-
-import scala.reflect.internal.util.BatchSourceFile
-import scala.tools.nsc.{GenericRunnerSettings, Global}
+import scala.reflect.runtime.currentMirror
+import scala.tools.reflect.ToolBox
 
 object Compilation {
-  val settings = new GenericRunnerSettings(System.out.println _)
-  val sbtClasspath = System.getProperty("sbt-classpath")
-  val s = File.pathSeparator
-  val classPath = s".${s}$sbtClasspath"
-  settings.classpath.append(classPath)
-  settings.usejavacp.value = true
-  val global = new Global(settings)
 
   def compiles(code: String): Boolean = {
-    val r = new global.Run
-    r.compileSources(List(new BatchSourceFile("<partest>", code)))
-    val errors = global.reporter.hasErrors
-    if (errors) r.reporting.summarizeErrors()
-    !errors
+    val toolBox = currentMirror.mkToolBox()
+    try {
+      toolBox.parse(code)
+      true
+    } catch {
+      case e: Throwable => {
+        e.printStackTrace()
+        false
+      }
+    }
   }
 }
