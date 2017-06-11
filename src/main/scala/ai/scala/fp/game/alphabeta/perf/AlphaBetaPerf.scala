772518b404c0b196dfe239538b88a046e69d8f26
Michel Daviot <michel@daviot.info>
2017-06-11 21:42:31 +0200

    alphabeta perf tests on Gomoku with a real heuristic

--- src/main/scala/ai/scala/fp/game/alphabeta/perf/AlphaBetaPerf.scala
+++ src/main/scala/ai/scala/fp/game/alphabeta/perf/AlphaBetaPerf.scala
@@ -1,7 +1,9 @@
 package ai.scala.fp.game.alphabeta.perf
 
 import ai.scala.fp.game.alphabeta.AlphaBetaAi
+import ai.scala.fp.game.gomoku.GomokuRules
 import ai.scala.fp.game.{DummyBoard, DummyRules}
+import ai.scala.fp.geo.Pos
 import org.openjdk.jmh.annotations._
 
 import scala.util.Random
@@ -9,6 +11,8 @@ import scala.util.Random
 @State(Scope.Benchmark)
 class AlphaBetaPerf {
   val rules = DummyRules(20, 10)
+
+  def heuristic(s: DummyBoard): Double = Random.nextDouble
 
   @Benchmark
   def dummyDepth3(): Int = {
@@ -20,5 +24,16 @@ class AlphaBetaPerf {
     AlphaBetaAi(rules, heuristic).chooseMove(rules.initial, 5)
   }
 
-  def heuristic(s: DummyBoard): Double = Random.nextDouble
+  val rules74 = GomokuRules(7, 4)
+  val rules33 = GomokuRules(3,3)
+
+  @Benchmark
+  def gomoku74Depth2(): Pos = {
+    AlphaBetaAi(rules74, rules74.centerHeuristic).chooseMove(rules74.initial, 2)
+  }
+
+  @Benchmark
+  def gomoku33Depth9(): Pos = {
+    AlphaBetaAi(rules33, rules33.centerHeuristic).chooseMove(rules33.initial, 9)
+  }
 }
