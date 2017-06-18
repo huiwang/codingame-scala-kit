188ce3980a491bf7b9a4ffac65597afc637fc116
Hui Wang <hedonplay@gmail.com>
2017-06-18 22:44:50 +0200

    add tic-tac-toe problem and unit test

---
+++ src/test/scala/com/truelaurel/problem/tictactoe/minimax/TicTacToeMinimaxTest.scala
@@ -0,0 +1,35 @@
+package com.truelaurel.problem.tictactoe.minimax
+
+import com.truelaurel.codingame.minimax.MinimaxAlgorithm
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Created by hwang on 18/06/2017.
+  */
+class TicTacToeMinimaxTest extends FlatSpec with Matchers {
+
+  behavior of "TicTacToeMinimaxTest"
+
+  it should "it should find the move which leads to victory" in {
+
+    val winningActions = Set(TicTacToeAction(O, 0), TicTacToeAction(O, 2))
+    val root = TicTacToeState(Vector(
+      -, O, -
+    ,
+    X
+    , O
+    , X
+    ,
+    -, X
+    , -
+    ), OPlayer
+    )
+
+    val repr = new TicTacToeMinimaxRepresentation(root)
+    val minimax = new MinimaxAlgorithm[TicTacToeState, TicTacToeAction]()
+
+    val suggestedAction = minimax.search(repr, 4)
+    winningActions.contains(suggestedAction) shouldBe true
+  }
+
+}
