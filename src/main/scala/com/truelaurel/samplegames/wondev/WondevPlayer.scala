c3c036c101737ff8925a7cd3490807da720ccc3d
[a518291] Michel Daviot <michel.daviot@atos.net>
2017-06-23 17:56:04 +0200

    I forgot that jmh does not like trait App

--- src/main/scala/com/truelaurel/samplegames/wondev/WondevPlayer.scala
+++ src/main/scala/com/truelaurel/samplegames/wondev/WondevPlayer.scala
@@ -7,9 +7,11 @@ import com.truelaurel.samplegames.wondev.strategy.NilPlayer
 
 // ~runMain com.truelaurel.codingame.tool.bundle.BundlerMain WondevPlayer.scala
 
-object Player extends App {
-  CGLogger.current = CGLogger.info
-  val gameLoop = new GameLoop(WondevController, NilPlayer(true))
-  gameLoop.run()
+object Player {
+  def main(args: Array[String]): Unit = {
+    CGLogger.current = CGLogger.info
+    val gameLoop = new GameLoop(WondevController, NilPlayer(true))
+    gameLoop.run()
+  }
 }
 
