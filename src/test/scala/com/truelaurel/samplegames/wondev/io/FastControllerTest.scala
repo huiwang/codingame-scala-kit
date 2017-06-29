b78c34653e379b02bc1ec47f3bb4198ba3a89468
Michel Daviot <michel@daviot.info>
2017-06-29 21:53:37 +0200

    WIP: keep track of past units pos to improve guessing

--- src/test/scala/com/truelaurel/samplegames/wondev/io/FastControllerTest.scala
+++ src/test/scala/com/truelaurel/samplegames/wondev/io/FastControllerTest.scala
@@ -14,9 +14,24 @@ class FastControllerTest extends FlatSpec with Matchers {
     ".02330.",
     "..211..",
     "...0...")
+
   "FastController" should "guess next state" in {
     val guessed = FastController(7).guessState(FastContext(7, 2), rows, Seq(Pos(3, 3), Pos(3, 4)), Seq(Pos(-1, -1), Pos(-1, -1)))
     val possibleOps = Array(3, 9, 10, 11, 15, 19, 21, 22, 26, 27, 29, 33, 45)
     guessed.possibleOpUnits shouldBe Array(possibleOps, possibleOps)
+
+    val rows2 = Seq(
+      "...0...",
+      "..123..",
+      ".01121.",
+      "0101412",
+      ".02330.",
+      "..211..",
+      "...0...")
+
+    val context2 = FastContext(7, 2, Some(guessed.copy(possibleOpUnits = Array(Array(3, 9), Array(15)))))
+    val guessed2 = FastController(7).guessState(context2, rows2, Seq(Pos(3, 3), Pos(3, 4)), Seq(Pos(-1, -1), Pos(-1, -1)))
+    guessed2.possibleOpUnits shouldBe Array(Array(3), Array(15))
+    guessed2.opUnits shouldBe Array(3, 15)
   }
 }
