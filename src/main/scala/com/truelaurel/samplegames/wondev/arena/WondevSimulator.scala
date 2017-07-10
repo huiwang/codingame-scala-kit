92d1b90261d29e0ed4ef5bbc7888a11ffd30c8ad
Michel Daviot <michel@daviot.info>
2017-07-10 22:40:08 +0200

    trait Simulator is not needed

--- src/main/scala/com/truelaurel/samplegames/wondev/arena/WondevSimulator.scala
+++ src/main/scala/com/truelaurel/samplegames/wondev/arena/WondevSimulator.scala
@@ -1,14 +1,13 @@
 package com.truelaurel.samplegames.wondev.arena
 
-import com.truelaurel.codingame.challenge.GameSimulator
 import com.truelaurel.samplegames.wondev.analysis.WondevAnalysis
 import com.truelaurel.samplegames.wondev.domain.{MoveBuild, MovePush, WondevAction, WondevState}
 
 /**
   * Created by hwang on 24/06/2017.
   */
-object WondevSimulator extends GameSimulator[WondevState, WondevAction] {
-  override def simulate(fromState: WondevState, actions: Vector[WondevAction]): WondevState = {
+object WondevSimulator {
+  def simulate(fromState: WondevState, actions: Vector[WondevAction]): WondevState = {
     val action = actions.head
     action match {
       case MoveBuild(unitIndex, moveDir, buildDir) =>
