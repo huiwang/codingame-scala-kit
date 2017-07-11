d466e8eedb51469d152eafb63b804bd1b9453619
Michel Daviot <michel@daviot.info>
2017-07-11 23:27:27 +0200

    fix test

--- src/test/scala/com/truelaurel/samplegames/stirfry/CardTest.scala
+++ src/test/scala/com/truelaurel/samplegames/stirfry/CardTest.scala
@@ -8,10 +8,10 @@ class CardTest extends FlatSpec with Matchers {
     Card.score(Seq(Shrimp, Soja, Ginger, Onion, Noodles)) shouldBe 22
   }
 
-  "best meal" should "score 30" in {
-    val best = Card.all.toSeq.combinations(5).maxBy(Card.score)
+  "best meal" should "score 24" in {
+    val best = Card.all.toSeq.combinations(5).filter(_.contains(Noodles)).maxBy(Card.score)
     println(best)
-    Card.score(best) shouldBe 30
+    Card.score(best) shouldBe 24
   }
 
   "best veg meal" should "score 11" in {
