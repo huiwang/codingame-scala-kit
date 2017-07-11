451e891511dae078e094a4969ba7827e8cb5a60e
[a518291] Michel Daviot <michel.daviot@atos.net>
2017-07-11 13:40:06 +0200

    add reader/writer implicits to have external dependencies and easier testability

--- src/main/scala/com/truelaurel/samplegames/wondev/io/WondevIO.scala
+++ src/main/scala/com/truelaurel/samplegames/wondev/io/WondevIO.scala
@@ -1,37 +1,39 @@
 package com.truelaurel.samplegames.wondev.io
 
-import com.truelaurel.codingame.challenge.GameIO
+import com.truelaurel.codingame.challenge.{ConsoleReader, ConsoleWriter, GameIO}
 import com.truelaurel.math.geometry.{Direction, Pos}
 import com.truelaurel.samplegames.wondev.domain._
 
-import scala.io.StdIn._
 
 object WondevIO extends GameIO[WondevState, WondevAction] {
-  def readInitialState(): WondevState = {
-    val size = readInt
-    val unitsperplayer = readInt
+  def readInitialState()(implicit reader: ConsoleReader): WondevState = {
+    val size = reader.readInt()
+    val unitsperplayer = reader.readInt()
     val context = WondevContext(size, unitsperplayer)
     read(context)
   }
 
-  def readState(turn: Int, previous: WondevState): WondevState =
-    read(previous.context).copy(turn = turn)
+  def readState(turn: Int, previous: WondevState)(implicit reader: ConsoleReader): WondevState =
+    read(previous.context)(reader).copy(turn = turn)
 
-  def read(context: WondevContext): WondevState = {
+  def writeAction(action: WondevAction)(implicit writer: ConsoleWriter): Unit =
+    writer.println(action)
+
+  private def read(context: WondevContext)(implicit reader: ConsoleReader): WondevState = {
     import context._
 
-    val rows = Seq.fill(size)(readLine)
+    val rows = Seq.fill(size)(reader.readLine())
     val myUnits = Seq.fill(unitsperplayer) {
-      val Array(unitx, unity) = for (i <- readLine split " ") yield i.toInt
+      val Array(unitx, unity) = for (i <- reader.readLine() split " ") yield i.toInt
       Pos(unitx, unity)
     }
     val opUnits = Seq.fill(unitsperplayer) {
-      val Array(unitx, unity) = for (i <- readLine split " ") yield i.toInt
+      val Array(unitx, unity) = for (i <- reader.readLine() split " ") yield i.toInt
       Pos(unitx, unity)
     }
 
-    val legalactions = Seq.fill(readInt) {
-      val Array(_type, _index, dir1, dir2) = readLine split " "
+    val legalactions = Seq.fill(reader.readInt()) {
+      val Array(_type, _index, dir1, dir2) = reader.readLine() split " "
       val index = _index.toInt
       if (_type == "MOVE&BUILD") {
         MoveBuild(index, Direction(dir1), Direction(dir2))
@@ -48,7 +50,6 @@ object WondevIO extends GameIO[WondevState, WondevAction] {
     WondevState(context, 0, heights, myUnits, opUnits, legalactions)
   }
 
-  def writeAction(action: WondevAction): Unit = ???
 }
 
 
