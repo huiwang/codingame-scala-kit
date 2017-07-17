87c3a8404683d4f06cb047b7dcb3e6eb62ddf958
Wei Chen <longlyeagle@gmail.com>
2017-07-17 13:37:27 +0700

    MuPlusLambdaTest

---
+++ src/test/scala/com/truelaurel/algorithm/metaheuristic/evolutionstrategy/MuPlusLambdaTest.scala
@@ -0,0 +1,22 @@
+package com.truelaurel.algorithm.metaheuristic.evolutionstrategy
+
+import com.truelaurel.time.CountStopper
+import org.scalatest.{FlatSpec, Matchers}
+
+
+class MuPlusLambdaTest extends FlatSpec with Matchers {
+
+  behavior of "MuPlusLambda"
+
+  val muSize = 20
+  val popSize = 100
+  val stopper = new CountStopper(1000)
+
+  val mpl = new MuPlusLambda(muSize, popSize, stopper)
+
+  it can "search for a good angle to throw a rock far" in {
+    val result = mpl.search[RockThrowingSolution](new RockThrowingProblem)
+    (result.degree - 45.0) should be < 1.0
+  }
+
+}
