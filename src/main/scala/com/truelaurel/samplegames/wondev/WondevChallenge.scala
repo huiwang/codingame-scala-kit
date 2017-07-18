7c973638e51d94f418f553e608cb1eb682e15637
Hui Wang <hedonplay@gmail.com>
2017-07-18 23:00:21 +0200

    add warmup and benchmark

--- src/main/scala/com/truelaurel/samplegames/wondev/WondevChallenge.scala
+++ src/main/scala/com/truelaurel/samplegames/wondev/WondevChallenge.scala
@@ -7,6 +7,7 @@ import com.truelaurel.samplegames.wondev.arena.WondevArena
 import com.truelaurel.samplegames.wondev.domain.{MoveBuild, WondevAction, WondevState}
 import com.truelaurel.samplegames.wondev.io.WondevIO
 import com.truelaurel.samplegames.wondev.strategy.MinimaxPlayer
+import com.truelaurel.samplegames.wondev.warmup.WondevWarmup
 
 // ~runMain com.truelaurel.codingame.tool.bundle.BundlerMain WondevChallenge.scala
 // cp -r target/universal/stage/ brutaltester/head
@@ -24,34 +25,11 @@ object Player {
     var previousAction: WondevAction = null
     var previousOppoScope: Set[Set[Pos]] = null
 
-    val warmupState = WondevState(5,
-      heightMap = Map(
-        Pos(0, 2) -> 0, Pos(0, 0) -> 0, Pos(4, 0) -> 0, Pos(3, 4) -> 0, Pos(3, 1) -> 0,
-        Pos(4, 1) -> 0, Pos(2, 0) -> 0, Pos(0, 3) -> 0, Pos(4, 4) -> 0, Pos(3, 0) -> 0,
-        Pos(1, 1) -> 0, Pos(1, 4) -> 0, Pos(0, 4) -> 0, Pos(3, 2) -> 0, Pos(1, 3) -> 0,
-        Pos(2, 2) -> 0, Pos(4, 2) -> 0, Pos(2, 4) -> 0, Pos(0, 1) -> 0, Pos(3, 3) -> 0,
-        Pos(2, 3) -> 0, Pos(1, 2) -> 0, Pos(2, 1) -> 0, Pos(4, 3) -> 0, Pos(1, 0) -> 0),
-      units = List(Pos(0, 4), Pos(1, 0), Pos(0, 0), Pos(-1, -1)),
-      legalActions = List(
-        MoveBuild(0, Pos(1, 4), Pos(2, 4)), MoveBuild(0, Pos(1, 4), Pos(1, 3)), MoveBuild(0, Pos(1, 4), Pos(2, 3)),
-        MoveBuild(0, Pos(1, 4), Pos(0, 3)), MoveBuild(0, Pos(1, 4), Pos(0, 4)), MoveBuild(0, Pos(0, 3), Pos(1, 3)),
-        MoveBuild(0, Pos(0, 3), Pos(0, 2)), MoveBuild(0, Pos(0, 3), Pos(1, 2)), MoveBuild(0, Pos(0, 3), Pos(0, 4)),
-        MoveBuild(0, Pos(0, 3), Pos(1, 4)), MoveBuild(0, Pos(1, 3), Pos(2, 3)), MoveBuild(0, Pos(1, 3), Pos(1, 2)),
-        MoveBuild(0, Pos(1, 3), Pos(2, 2)), MoveBuild(0, Pos(1, 3), Pos(0, 2)), MoveBuild(0, Pos(1, 3), Pos(1, 4)),
-        MoveBuild(0, Pos(1, 3), Pos(2, 4)), MoveBuild(0, Pos(1, 3), Pos(0, 4)), MoveBuild(0, Pos(1, 3), Pos(0, 3)),
-        MoveBuild(1, Pos(2, 0), Pos(3, 0)), MoveBuild(1, Pos(2, 0), Pos(2, 1)), MoveBuild(1, Pos(2, 0), Pos(3, 1)),
-        MoveBuild(1, Pos(2, 0), Pos(1, 1)), MoveBuild(1, Pos(2, 0), Pos(1, 0)), MoveBuild(1, Pos(1, 1), Pos(2, 1)),
-        MoveBuild(1, Pos(1, 1), Pos(1, 0)), MoveBuild(1, Pos(1, 1), Pos(2, 0)), MoveBuild(1, Pos(1, 1), Pos(1, 2)),
-        MoveBuild(1, Pos(1, 1), Pos(2, 2)), MoveBuild(1, Pos(1, 1), Pos(0, 2)), MoveBuild(1, Pos(1, 1), Pos(0, 1)),
-        MoveBuild(1, Pos(2, 1), Pos(3, 1)), MoveBuild(1, Pos(2, 1), Pos(2, 0)), MoveBuild(1, Pos(2, 1), Pos(3, 0)),
-        MoveBuild(1, Pos(2, 1), Pos(1, 0)), MoveBuild(1, Pos(2, 1), Pos(2, 2)), MoveBuild(1, Pos(2, 1), Pos(3, 2)),
-        MoveBuild(1, Pos(2, 1), Pos(1, 2)), MoveBuild(1, Pos(2, 1), Pos(1, 1)), MoveBuild(1, Pos(0, 1), Pos(1, 1)),
-        MoveBuild(1, Pos(0, 1), Pos(1, 0)), MoveBuild(1, Pos(0, 1), Pos(0, 2)), MoveBuild(1, Pos(0, 1), Pos(1, 2))),
-      nextPlayer = true)
-
-    WarFogAnalysis.restrictOppoScope(warmupState, previousState, previousAction, previousOppoScope)
-    MinimaxPlayer.react(warmupState)
-
+    val time = System.nanoTime()
+    for ( i <- 0 until 5) {
+      WondevWarmup.warmup()
+    }
+    System.err.println("warmup elt: " + (System.nanoTime() - time) / 1000000 + "ms")
 
     try {
       while (true) {
