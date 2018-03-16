b59737814b1e1c317ba713f8a773d634ad038fdd
Michel Daviot <michel@daviot.info>
2018-03-16 21:35:06 +0100

    :sparkles: Circle class which computes intersections with another Circle

---
+++ src/main/scala/com/truelaurel/math/geometry/Circle.scala
@@ -0,0 +1,43 @@
+package com.truelaurel.math.geometry
+
+import scala.math.sqrt
+
+case class Circle(center: Pos, radius: Int) {
+  def sqr(x: Double) = x * x
+
+  // http://nains-games.over-blog.com/2014/12/intersection-de-deux-cercles.html
+  // https://pastebin.com/5hT3G3V8
+  def intersections(ci: Circle): Seq[Pos] = {
+    val rc1 = radius
+    val rc2 = ci.radius
+    val xc1 = center.x
+    val yc1 = center.y
+    val xc2 = ci.center.x
+    val yc2 = ci.center.y
+
+    if (yc1 == yc2) {
+      val a = (xc1 - xc2).abs
+      val XIa = (sqr(rc2) - sqr(a) - sqr(rc1)) / (-2 * a)
+      val root = sqr(rc2) - sqr(a - XIa)
+      if (root >= 0) {
+        val YIa = yc1 + sqrt(root)
+        val YIb = yc1 - sqrt(root)
+        Seq(new Pos(XIa, YIa), new Pos(XIa, YIb)).distinct
+      } else Seq.empty
+    } else {
+      val a = (-sqr(xc1) - sqr(yc1) + sqr(xc2) + sqr(yc2) + sqr(rc1) - sqr(rc2)) / (2 * (yc2 - yc1))
+      val d = (xc2.toDouble - xc1) / (yc2 - yc1)
+      val A = sqr(d) + 1
+      val B = -2 * xc1 + 2 * yc1 * d - 2 * a * d
+      val C = sqr(xc1) + sqr(yc1) - 2 * yc1 * a + sqr(a) - sqr(rc1)
+      val delta = sqr(B) - 4 * A * C
+      if (delta >= 0) {
+        val XIa = (-B + sqrt(delta)) / (2 * A)
+        val XIb = (-B - sqrt(delta)) / (2 * A)
+        val YIa = a - ((-B + sqrt(delta)) / (2 * A)) * d
+        val YIb = a - ((-B - sqrt(delta)) / (2 * A)) * d
+        Seq(new Pos(XIa, YIa), new Pos(XIb, YIb)).distinct
+      } else Seq.empty
+    }
+  }
+}
---
+++ src/test/scala/com/truelaurel/math/geometry/CircleTest.scala
@@ -0,0 +1,26 @@
+package com.truelaurel.math.geometry
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class CircleTest extends FlatSpec with Matchers {
+
+  "intersection" should "be empty for circles too far away" in {
+    Circle(Pos(100, 150), 100).intersections(Circle(Pos(500, 150), 100)) shouldBe empty
+  }
+
+  it should "be a single point when tangent" in {
+    Circle(Pos(0, 0), 100).intersections(Circle(Pos(200, 0), 100)) shouldBe Seq(Pos(100, 0))
+    Circle(Pos(0, 400), 200).intersections(Circle(Pos(600, 400), 400)) shouldBe Seq(Pos(200, 400))
+  }
+
+  it should "be a 2 points when secant" in {
+    Circle(Pos(0, 0), 100).intersections(Circle(Pos(0, 75), 100)) should
+      contain theSameElementsAs Seq(Pos(92, 37), Pos(-92, 37))
+
+    Circle(Pos(0, 50), 100).intersections(Circle(Pos(50, 0), 100)) should
+      contain theSameElementsAs Seq(Pos(91, 91), Pos(-41, -41))
+    //detects rounding bug
+    Circle(Pos(1735, 542), 180).intersections(Circle(Pos(1476, 490), 220)) should
+      contain theSameElementsAs List(Pos(1664, 376), Pos(1605, 667))
+  }
+}
